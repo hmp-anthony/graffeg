8e2c9521776c97b0c29645c465f386a7025351b9
hmp-anthony <anthonygoddard962@gmail.com>
2024-04-25 18:18:01 +0100

    finished bvh optim. starting final optim.

--- src/example.cpp
+++ src/example.cpp
@@ -5,6 +5,7 @@
 #include <graffeg/sphere.hpp>
 #include <graffeg/camera.hpp>
 #include <graffeg/material.hpp>
+#include <graffeg/bvh.hpp>
 
 #include <iostream>
 
@@ -52,10 +53,12 @@ int main() {
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
+    world = hittable_list(make_shared<bvh_node>(world));
+
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
+    cam.image_width       = 800;
     cam.samples_per_pixel = 10;
     cam.max_depth         = 5;
 
