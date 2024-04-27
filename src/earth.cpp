ca57d114092a315f40a9bc0b755d3fdcecc7692c
hmp-anthony <anthonygoddard962@gmail.com>
2024-04-27 15:18:53 +0100

    starting section 5: Perlin Noise

---
+++ src/earth.cpp
@@ -0,0 +1,32 @@
+#include <graffeg/graffeg.hpp>
+
+#include <graffeg/hittable.hpp>
+#include <graffeg/hittable_list.hpp>
+#include <graffeg/sphere.hpp>
+#include <graffeg/camera.hpp>
+#include <graffeg/material.hpp>
+#include <graffeg/bvh.hpp>
+
+#include <iostream>
+
+int main() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(0,0,12);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
