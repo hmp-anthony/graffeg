32a75e2c2612e3d6292c95c7a340ceecb37d6578
hmp-anthony <anthonygoddard962@gmail.com>
2024-04-28 14:43:08 +0100

    finished perlin textures

--- include/graffeg/texture.hpp
+++ include/graffeg/texture.hpp
@@ -82,7 +82,7 @@ public:
     noise_texture() {}
     noise_texture(double scale) : scale(scale) {}
     color value(double u, double v, const point3& p) const override {
-        return color(1, 1, 1) * 0.5 * (1.0 + noise.noise(scale * p));
+        return color(.5, .5, .5) * (1 + sin(scale * p.z() + 10 * noise.turb(p, 7)));
     }
 private:
     perlin noise;
